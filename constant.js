d4bbd2b573301349d61d696f4bd34964bf73e35a
fahrezi <luthfi0071@gmail.com>
2025-07-11 16:07:17 +0700

    first commit

---
+++ constant.js
@@ -0,0 +1,55 @@
+const CATEGORY = {
+  technology: {
+    title: 'ASA TECHNOLOGY',
+    link: 'https://asakreasi.co.id/asa-technology.html',
+    services: 'Our ICT solutions deliver robust Telco Infra Services, seamless System Integration, and reliable Build & Manage capabilities to drive your business forward. Backed by expert ICT Consultancy, we ensure tailored strategies for optimal performance and innovation.',
+    products: [
+      {
+        image: './assets/tech-product-1.png',
+        text: 'ICT Consultant'
+      },
+      {
+        image: './assets/tech-product-2.png',
+        text: 'Telecommunications Infrastructure Services'
+      }
+    ]
+  },
+  creative: {
+    title: 'ASA CREATIVE',
+    link: 'https://asakreasi.co.id/asa-creative.html',
+    services: 'Our digital IoT products enable real-time monitoring, automation, and data-driven insights. Designed to enhance operational efficiency and connectivity, they empower businesses to stay ahead in a connected world.',
+    products: [
+      {
+        image: './assets/creative-product-1.png',
+        text: 'Digital Human'
+      },
+      {
+        image: './assets/creative-product-2.png',
+        text: 'Voice Cloning'
+      },
+      {
+        image: './assets/creative-product-3.png',
+        text: 'Digital Product'
+      },
+    ]
+  },
+  edutech: {
+    title: 'ASA EDU-TECH',
+    link: 'https://asakreasi.co.id/asa-edutech.html',
+    services: 'We provide customized training, certification, and assessment services to support skill development and organizational excellence.',
+    products: [
+      {
+        image: './assets/edutech-product-1.png',
+        text: 'Allsmart'
+      },
+      {
+        image: './assets/edutech-product-2.png',
+        text: 'Manoosia'
+      },
+      {
+        image: './assets/edutech-product-3.png',
+        text: 'Pondasi Digibook'
+      }
+    ]
+  },
+}
